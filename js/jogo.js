1b03fa98bb33e534986afd794fd3d49131051ee3
antonioaugs0505 <rm550344@fiap.com.br>
2024-06-14 12:49:49 -0300

    Conclusão de placar

--- js/jogo.js
+++ js/jogo.js
@@ -16,72 +16,137 @@ $(document).ready(function() {
   $('#pontosDupla1').text(pontosDupla1);
   $('#pontosDupla2').text(pontosDupla2);
 
+  // Inicializar games das duplas
+  var gamesDupla1 = 0;
+  var gamesDupla2 = 0;
+  atualizaGames();
+
   // Inicializar sets das duplas
   var setsDupla1 = 0;
   var setsDupla2 = 0;
   atualizaSets();
 
-  // Ação ao clicar em um botão de jogador
-  $('.jogadores-buttons button').click(function() {
-    var jogador = $(this).text();
-    alert(`Popup do jogador ${jogador}`);
-    // Aqui você pode adicionar lógica adicional conforme necessário
-  });
-
   // Ação ao clicar nos botões de placar
   $('#btnMaisDupla1').click(function() {
-    pontosDupla1++;
+    pontosDupla1 = proximaPontuacao(pontosDupla1);
     atualizaPlacar();
-    verificaFimDoSet();
+    verificaFimDoGame(1);
   });
 
   $('#btnMenosDupla1').click(function() {
-    if (pontosDupla1 > 0) {
-      pontosDupla1--;
-      atualizaPlacar();
+    if (pontosDupla1 === 40) {
+      pontosDupla1 = 30;
+    } else if (pontosDupla1 === 30) {
+      pontosDupla1 = 15;
+    } else if (pontosDupla1 === 15) {
+      pontosDupla1 = 0;
     }
+    atualizaPlacar();
   });
 
   $('#btnMaisDupla2').click(function() {
-    pontosDupla2++;
+    pontosDupla2 = proximaPontuacao(pontosDupla2);
     atualizaPlacar();
-    verificaFimDoSet();
+    verificaFimDoGame(2);
   });
 
   $('#btnMenosDupla2').click(function() {
-    if (pontosDupla2 > 0) {
-      pontosDupla2--;
-      atualizaPlacar();
+    if (pontosDupla2 === 40) {
+      pontosDupla2 = 30;
+    } else if (pontosDupla2 === 30) {
+      pontosDupla2 = 15;
+    } else if (pontosDupla2 === 15) {
+      pontosDupla2 = 0;
     }
+    atualizaPlacar();
   });
 
-  function atualizaPlacar() {
-    $('#pontosDupla1').text(pontosDupla1);
-    $('#pontosDupla2').text(pontosDupla2);
+  function proximaPontuacao(pontos) {
+    if (pontos === 0) return 15;
+    else if (pontos === 15) return 30;
+    else if (pontos === 30) return 40;
+    else if (pontos === 40) return 'AD';
+    else if (pontos === 'AD') return 0;
   }
 
-  function verificaFimDoSet() {
-    if (pontosDupla1 >= 21 && pontosDupla1 >= pontosDupla2 + 2) {
-      alert(`Set finalizado! A dupla ${partidaAtual.dupla1} venceu o set.`);
-      setsDupla1++;
-      reiniciarSet();
-      atualizaSets();
-    } else if (pontosDupla2 >= 21 && pontosDupla2 >= pontosDupla1 + 2) {
-      alert(`Set finalizado! A dupla ${partidaAtual.dupla2} venceu o set.`);
-      setsDupla2++;
-      reiniciarSet();
-      atualizaSets();
+  function atualizaPlacar() {
+    $('#pontosDupla1').text(pontosDupla1 === 40 ? '40' : pontosDupla1);
+    $('#pontosDupla2').text(pontosDupla2 === 40 ? '40' : pontosDupla2);
+  }
+
+  function verificaFimDoGame(dupla) {
+    if (dupla === 1 && pontosDupla1 === 'AD') {
+      gamesDupla1++;
+      atualizaGames();
+      $('#modalVitoriaGameBody').text(`Game finalizado! A dupla ${partidaAtual.dupla1} venceu o game.`);
+      $('#modalVitoriaGame').modal('show');
+      reiniciarGame();
+      verificaFimDoSet();
+    } else if (dupla === 2 && pontosDupla2 === 'AD') {
+      gamesDupla2++;
+      atualizaGames();
+      $('#modalVitoriaGameBody').text(`Game finalizado! A dupla ${partidaAtual.dupla2} venceu o game.`);
+      $('#modalVitoriaGame').modal('show');
+      reiniciarGame();
+      verificaFimDoSet();
     }
   }
 
-  function reiniciarSet() {
+  function reiniciarGame() {
     pontosDupla1 = 0;
     pontosDupla2 = 0;
     atualizaPlacar();
   }
 
+  function verificaFimDoSet() {
+    if (gamesDupla1 >= 6 && gamesDupla1 >= gamesDupla2 + 2) {
+      setsDupla1++;
+      atualizaSets();
+      reiniciarSet();
+      $('#modalVitoriaSetBody').text(`Set finalizado! A dupla ${partidaAtual.dupla1} venceu o set.`);
+      $('#modalVitoriaSet').modal('show');
+    } else if (gamesDupla2 >= 6 && gamesDupla2 >= gamesDupla1 + 2) {
+      setsDupla2++;
+      atualizaSets();
+      reiniciarSet();
+      $('#modalVitoriaSetBody').text(`Set finalizado! A dupla ${partidaAtual.dupla2} venceu o set.`);
+      $('#modalVitoriaSet').modal('show');
+    } else if (gamesDupla1 === 7 && gamesDupla2 === 6) {
+      setsDupla1++;
+      atualizaSets();
+      reiniciarSet();
+      $('#modalVitoriaSetBody').text(`Set finalizado! A dupla ${partidaAtual.dupla1} venceu o set.`);
+      $('#modalVitoriaSet').modal('show');
+    } else if (gamesDupla2 === 7 && gamesDupla1 === 6) {
+      setsDupla2++;
+      atualizaSets();
+      reiniciarSet();
+      $('#modalVitoriaSetBody').text(`Set finalizado! A dupla ${partidaAtual.dupla2} venceu o set.`);
+      $('#modalVitoriaSet').modal('show');
+    } else if (gamesDupla1 === 7 && gamesDupla2 === 7) {
+      setsDupla1++;
+      setsDupla2++;
+      atualizaSets();
+      reiniciarSet();
+      $('#modalVitoriaSetBody').text(`Set finalizado! Ambas as duplas empataram e venceram o set.`);
+      $('#modalVitoriaSet').modal('show');
+    }
+  }
+
+  function reiniciarSet() {
+    gamesDupla1 = 0;
+    gamesDupla2 = 0;
+    atualizaGames();
+  }
+
+  function atualizaGames() {
+    $('#gamesDupla1').text(gamesDupla1);
+    $('#gamesDupla2').text(gamesDupla2);
+  }
+
   function atualizaSets() {
-    $('#sets').html(`<div class="text-center d-flex justify-content-center align-center"><h5 style="font-size: 1.8rem;" class="mx-2 badge badge-primary">${setsDupla1}</h5> <h5 style="font-size: 1.8rem;">-</h5><h5 style="font-size: 1.8rem;" class=" mx-2 badge badge-primary">${setsDupla2}</h5></div>`);
+    $('#setsDupla1').text(setsDupla1);
+    $('#setsDupla2').text(setsDupla2);
   }
 
   function loadJogadores(nomeDupla, listaId) {
